0d44ce6e54f6d441795bfad91b66490300ea36ba
TreeGee73 <TreeGee73@gmail.com>
2020-12-07 21:27:02 -0700

    updates questions.js/app.js

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
+// Node Packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const questions = require("./questions");
+
+// Outside File Variables
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
+const questions = require("./questions");
 
+// Path Variables
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// Team Variable (Built by question answers)
+let teamData = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-inquirer.prompt(questions).then(function (data) {
-  console.log(data);
-});
+
+
+// Prompt for Employee to Add (Manager or )
+
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- questions.js
+++ questions.js
@@ -1,59 +1,50 @@
-const questionsEmp = [
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+module.exports = [
     {
         type: 'list',
         name: 'role',
-        message:"What is the employee's role?",
+        message:'What is the employee\'s role?',
         choices: ['Manager', 'Engineer', 'Intern']
     },
     {
         type:'text',
         name: 'employee',
-        message: "What is the Employee's name?"
+        message: 'What is the Employee\'s name?'
     },
     {
         type:'text',
         name: 'id',
-        message: "What is the employee's ID number?"
+        message: 'What is the employee\'s ID number?'
     },
     {
         type: 'text',
         name: 'email',
-        message: "What is the employee's email?"
+        message: 'What is the employee\'s email?'
     },
     {
         type:'confirm',
         name:'anotherEntry',
-        message: "What you like to add another employee?",
+        message: 'What you like to add another employee?',
         default: false
-    }
-    
-]
-
-const questionsMgr = [
+    },
     {
         type:'text',
         name: 'office',
-        message:"What is the Manager's office number?"
-    }
-    
-]
-
-const questionsEng = [
+        message:'What is the Manager\'s office number?'
+        when: (answers) => answers.role === 'Manager'
+    },
     {
         type: 'text',
         name: 'github',
-        message: "What is the Engineer's Github username?"
-    }
-    
-]
-
-const questionsInt = [
+        message: 'What is the Engineer\'s Github username?'
+        when: (answers) => answers.role === 'Engineer'
+    },
     {
         type:'text',
         name:'school',
-        message: "What is the Intern's school?"
-    }
-    
+        message: 'What is the Intern\'s school?'
+        when: (answers) => answers.role === 'Intern'
+    }   
 ]
-
-module.exports = questions;
