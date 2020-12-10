78b54ee7ad1d8398eee32956d6c6de276154984d
TreeGee73 <TreeGee73@gmail.com>
2020-12-09 22:00:20 -0700

    moved ?s to app.js, matched vars to render, html

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
-const Questions = require("./lib/Questions");
+// const Questions = require("./lib/Questions");
 
 // Path Variables
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -21,6 +21,102 @@ const teamData = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// Questions to be asked for the Manager Employee Type
+const mgrQuestions = [
+    {
+        type:'input',
+        name: 'name',
+        message: 'Hello, Team Manager! What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type:'input',
+        name: 'officeNumber',
+        message: 'What is your office number?'
+    },
+    {
+        type: 'list',
+        name: 'addEmp',
+        message: 'What type of Employee would you like to add to your team?',
+        choices: ['Engineer', 'Intern']
+    }
+];
+
+// Questions to be asked for the Engineer Employee Type
+const engQuestions = [
+    {
+        type:'input',
+        name: 'name',
+        message: 'What is the engineer\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the engineer\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the engineer\'s email?'
+    },
+    {
+        type:'input',
+        name: 'github',
+        message: 'What is the engineer\'s Github user name?'
+    },
+    {
+        type: 'list',
+        name: 'addEmp',
+        message: 'Would you like to add another employee to your team?',
+        choices: ['Engineer', 'Intern', 'Done']
+    }
+];
+
+// Questions to be asked for the Intern Employee Type
+const internQuestions = [
+    {
+        type:'input',
+        name: 'name',
+        message: 'What is the intern\'s name?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the intern\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the interns\'s email?'
+    },
+    {
+        type:'input',
+        name: 'school',
+        message: 'What is your intern\'s school?'
+    },
+    {
+        type: 'list',
+        name: 'addEmp',
+        message: 'Would you like to add another employee to your team?',
+        choices: ['Engineer', 'Intern', 'Done']
+    }
+];
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -30,7 +126,7 @@ inquirer
     // Starts by asking questions about the manager
     .prompt(mgrQuestions)
     // Construct Manager object from user input
-    .then(input => {teamData.push(new Manager(input.name, input.id, input.email, input.office));
+    .then(input => {teamData.push(new Manager(input.name, input.id, input.email, input.officeNumber));
         // Moves on to collect additional employee data or render collected data depending on user selection
         empType = input.addEmp;
             // If the user selection is to add an Engineer, will execute the getEngineer function
@@ -60,7 +156,7 @@ function getEngineer() {
         // Starts by asking questions about the engineer
         .prompt(engQuestions)
         // Construct Engineer object from user answers
-        .then(answer => {teamData.push(new Engineer(answer.name, answer.id, answer.email, answer.userName));
+        .then(answer => {teamData.push(new Engineer(answer.name, answer.id, answer.email, answer.github));
             // Moves on to collect additional employee data or render collected data depending on user selection
             empType = answer.addEmp;
             // If the user selection is to add an Engineer, will execute the getEngineer function
