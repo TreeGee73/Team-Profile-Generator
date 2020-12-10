e262791c132038390aa4edf54701dcf502be7764
TreeGee73 <TreeGee73@gmail.com>
2020-12-09 21:03:50 -0700

    Employee.js coded, questions tweaked, app.js reverted to initial file

--- app.js
+++ app.js
@@ -14,5 +14,18 @@ const questions = require("./lib/questions");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// Team Variable (Built by question answers)
-let teamData = [];
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+
+// Once application is invoked, data collection begins with a series of questions that once the user answers will be passed into the teamData array. The teamData array will contain all employee objects that will later be used with the render function to create a block of HTML that will contain divs that include the collected data for each employee.
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
