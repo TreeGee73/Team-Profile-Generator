55b27b103c583b72e2901be77fc10101c6b66886
TreeGee73 <TreeGee73@gmail.com>
2020-12-10 13:12:06 -0700

    Fixed external ?s files, updated app.js

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const fs = require("fs");
 
 // Outside File Variables
 const Manager = require("./lib/Manager");
+const mgrQuestions = require("./lib/MgrQuestions");
+
 const Engineer = require("./lib/Engineer");
+const engQuestions = require("./lib/EngQuestions");
+
 const Intern = require("./lib/Intern");
+const internQuestions = require("./lib/InternQuestions");
+
 const render = require("./lib/htmlRenderer");
-// const Questions = require("./lib/Questions");
 
 // Path Variables
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -21,103 +26,6 @@ const teamData = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// Questions to be asked for the Manager Employee Type
-const mgrQuestions = [
-    {
-        type:'input',
-        name: 'name',
-        message: 'Hello, Team Manager! What is your name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your id?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?'
-    },
-    {
-        type:'input',
-        name: 'officeNumber',
-        message: 'What is your office number?'
-    },
-    {
-        type: 'list',
-        name: 'addEmp',
-        message: 'What type of Employee would you like to add to your team?',
-        choices: ['Engineer', 'Intern']
-    }
-];
-
-// Questions to be asked for the Engineer Employee Type
-const engQuestions = [
-    {
-        type:'input',
-        name: 'name',
-        message: 'What is the engineer\'s name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the engineer\'s id?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the engineer\'s email?'
-    },
-    {
-        type:'input',
-        name: 'github',
-        message: 'What is the engineer\'s Github user name?'
-    },
-    {
-        type: 'list',
-        name: 'addEmp',
-        message: 'Would you like to add another employee to your team?',
-        choices: ['Engineer', 'Intern', 'Done']
-    }
-];
-
-// Questions to be asked for the Intern Employee Type
-const internQuestions = [
-    {
-        type:'input',
-        name: 'name',
-        message: 'What is the intern\'s name?'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is the intern\'s id?'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is the interns\'s email?'
-    },
-    {
-        type:'input',
-        name: 'school',
-        message: 'What is your intern\'s school?'
-    },
-    {
-        type: 'list',
-        name: 'addEmp',
-        message: 'Would you like to add another employee to your team?',
-        choices: ['Engineer', 'Intern', 'Done']
-    }
-];
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -125,7 +33,7 @@ const internQuestions = [
 inquirer
     // Starts by asking questions about the manager
     .prompt(mgrQuestions)
-    // Construct Manager object from user input
+    // Construct Manager object from user input and push it into the teamData array
     .then(input => {teamData.push(new Manager(input.name, input.id, input.email, input.officeNumber));
         // Moves on to collect additional employee data or render collected data depending on user selection
         empType = input.addEmp;
