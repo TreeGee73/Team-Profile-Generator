732216fa3ad03d87a434af6923e61d9833fca6dd
TreeGee73 <TreeGee73@gmail.com>
2020-12-09 21:40:31 -0700

    app.js, program initialization built

--- app.js
+++ app.js
@@ -8,21 +8,53 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const render = require("./lib/htmlRenderer");
-const questions = require("./lib/questions");
+// const Questions = require("./lib/Questions");
 
 // Path Variables
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Employee & Team variables
+let empType;
+const teamData = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+inquirer
+    // Starts by asking questions about the manager
+    .prompt(mgrQuestions)
+    // Construct Manager object from user input
+    .then(input => {teamData.push(new Manager(input.name, input.id, input.email, input.officeNumber));
+        // Moves on to collect additional employee data or render collected data depending on user selection
+        empType = input.addEmp;
+            // If the user selection is to add an Engineer, will execute the getEngineer function
+            if(empType === 'Engineer') {
+                getEngineer();
+            }
+            // If the user selection is to add an Intern, will execute the getIntern function
+            else if(empType === 'Intern') {
+                getIntern();
+            }
+            // If the user selection is done, will render the teamData array into HTML and execute the writeHTMLFile function to place the new HTML code in team.html for display on a browser
+            else {
+                const html = render(teamData);
+                writeHTMLFile(html);
+            }
+        })
+    // If promise should fail returns an error in the console log and ends/exits the program
+    .catch(error => {
+        console.log(error);
+        // To exit with a 'failure' code, the shell that executed Node.js should see the exit code as 1. Calling process.exit() will force the process to exit as quickly as possible even if there are still asynchronous operations pending that have not yet completed fully, including I/O operations to process.stdout and process.stderr. 
+        process.exit(1);
+    })
 
-// Once application is invoked, data collection begins with a series of questions that once the user answers will be passed into the teamData array. The teamData array will contain all employee objects that will later be used with the render function to create a block of HTML that will contain divs that include the collected data for each employee.
+// If user indicates the addition of an engineer this function is executed.
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
